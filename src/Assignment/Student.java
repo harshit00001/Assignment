b2c1ae5f99b744f301dd146cbfac1d75d744c79d
amdjfsim16 <amdjfsim16@AmdJava17.iiht.tech>
2022-09-19 02:04:30 +0530

    Initial Commit

--- src/Assignment/Student.java
+++ src/Assignment/Student.java
@@ -5,41 +5,52 @@ public class Student {
 	private int Roll_no;
 	private int Age;
 	private int Marks;
-	public String getName() {
-		return Name;
-	}
-	public void setName(String name) {
-		Name = name;
-	}
-	public int getRoll_no() {
-		return Roll_no;
-	}
-	public void setRoll_no(int roll_no) {
-		Roll_no = roll_no;
-	}
-	public int getAge() {
-		return Age;
-	}
-	public void setAge(int age) {
-		Age = age;
-	}
-	public int getMarks() {
-		return Marks;
-	}
-	public void setMarks(int marks) {
-		Marks = marks;
-	}
-	public void marks_Check(String Name,int Roll_no,int age,int marks)
+	public Student(String Name,int Roll_no,int Age,int Marks) 
 	{
-		this.Marks=marks;
+		this.Name=Name;
+		this.Roll_no=Roll_no;
+		this.Age=Age;
+		this.Marks=Marks;
 		if (Marks<30)
 		{
-			System.out.println("Name: " + Name + "\nRollNo: " + Roll_no + "\nage: " + age+ "\nMarks: " + marks + " ,Marks Less than 30 Cannot be accepted");
+			System.out.println("Name: " + Name + "\nRollNo: " + Roll_no + "\nage: " + Age+ "\nMarks: " + Marks + " ,Marks Less than 30 Cannot be accepted");
 		}
 		else
 		{
-			System.out.println("Name: " + Name + "\nRollNo: " + Roll_no + "\nage: " + age+ "\nMarks: " + marks +" ,Marks Acceptable ");
+			System.out.println("Name: " + Name + "\nRollNo: " + Roll_no + "\nage: " + Age+ "\nMarks: " + Marks +" ,Marks Acceptable ");
 		}
+	}
+	
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public int getRoll_no() {
+		return Roll_no;
+	}
+
+	public void setRoll_no(int roll_no) {
+		Roll_no = roll_no;
+	}
+
+	public int getAge() {
+		return Age;
+	}
+
+	public void setAge(int age) {
+		Age = age;
+	}
+
+	public int getMarks() {
+		return Marks;
+	}
+
+	public void setMarks(int marks) {
+		Marks = marks;
 	}
 
 }
--- src/Assignment/TestMain.java
+++ src/Assignment/TestMain.java
@@ -6,19 +6,13 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Student stud1=new Student();
-		Student stud2=new Student();
-		Student stud3=new Student();
-		Student stud4=new Student();
-		Student stud5=new Student();
-		Scanner in =new Scanner(System.in);
 		System.out.println("Enter 5 Student Marks: ");
-		stud1.marks_Check("Harsh",1,22,in.nextInt());
-		stud2.marks_Check("Aaddya",2,22,in.nextInt());
-		stud3.marks_Check("Abhay",3,21,in.nextInt());
-		stud3.marks_Check("Chetan",4,22,in.nextInt());
-		stud4.marks_Check("Debayan",5,22,in.nextInt());
-		stud5.marks_Check("preeti",6,22,in.nextInt());
+		Scanner in =new Scanner(System.in);
+		Student stud1=new Student("Harsh",1,22,in.nextInt());
+		Student stud2=new Student("Aaddya",2,22,in.nextInt());
+		Student stud3=new Student("Abhay",3,21,in.nextInt());
+		Student stud4=new Student("Chetan",4,22,in.nextInt());
+		Student stud5=new Student("Debayan",5,22,in.nextInt());
 		
 	}
 
