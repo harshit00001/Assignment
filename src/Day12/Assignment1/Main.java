91c86c44aa4392c6b33c6d0de6bde1170100fd85
amdjfsim16 <amdjfsim16@AmdJava17.iiht.tech>
2022-09-29 21:27:22 +0530

    Test2: Friday

---
+++ src/Day12/Assignment1/Main.java
@@ -0,0 +1,10 @@
+package Day12.Assignment1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Day12/Assignment2/Main2.java
@@ -0,0 +1,10 @@
+package Day12.Assignment2;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
